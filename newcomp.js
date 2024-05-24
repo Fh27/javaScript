91b61a18cae13a83116bbd6cc6f4c63730618874
Md Furqan Hasan <furqan.hasan27@gmail.com>
2024-05-24 05:53:11 +0530

    yes

---
+++ newcomp.js
@@ -0,0 +1,19 @@
+function add(a,b){
+    return a+b;
+}
+function multiply(args){
+    return args[0]*args[1]
+}
+function square(val){
+    return val*val
+}
+function compose(...fns){
+    return function(...values){
+        return fns.reduce((a,b)=>b(a),values)
+    }
+}
+const composeall=(...fns)=>(...val)=>fns.reduce((a,b)=> b(a),val);
+const tsak=composeall(multiply,square)
+const task=compose(add,multiply)
+// console.log(task(2,3))
+console.log(tsak(2,3))
